cbe94d256b48b633a8594acd51866a6620eb2986
jeremiah <bankolejeremiahadeoye@gmail.com>
2025-02-21 17:39:22 +0100

    done with the frontend interface

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,31 +2,31 @@ import React, { useEffect, useState } from 'react'
 
 const NavBar = () => {
 
-    const [bgColor, setBgColor] = useState("white");
+    // const [bgColor, setBgColor] = useState("white");
 
-    useEffect(() => {
-        const handleScroll = () => {
-        if (window.scrollY > 50) {
-            setBgColor("#fff");
-        } else {
-            setBgColor("#d0d0ce");
-        }
-        };
+    // useEffect(() => {
+    //     const handleScroll = () => {
+    //     if (window.scrollY > 50) {
+    //         setBgColor("#fff");
+    //     } else {
+    //         setBgColor("#d0d0ce");
+    //     }
+    //     };
 
-        window.addEventListener("scroll", handleScroll);
+    //     window.addEventListener("scroll", handleScroll);
 
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
+    //     return () => {
+    //         window.removeEventListener("scroll", handleScroll);
+    //     };
+    // }, []);
 
     return (
         <div 
-            style={{backgroundColor: bgColor}}
-            className='flex justify-between w-full py-4 fixed top-0 left-0 px-3 md:px-6 lg:px-12'
+            // style={{backgroundColor: bgColor}}
+            className='flex justify-between w-full py-4 fixed top-0 left-0 px-3 md:px-6 lg:px-12 bg-[#d0d0ce]'
         >
             <div className="road-rage-regular text-4xl flex items-end">TextPT <span className='relative bottom-2 block font-bold w-2 h-2 bg-[#1d4537] rounded-full'></span></div>
-            <button className='text-[#1d4537] hover:text-gray-700 text-lg font-bold transition-all duration-500'>Explore</button>
+            <button className='text-[#1d4537] hover:bg-[#1d4537] hover:text-white px-4 rounded-md text-lg font-bold transition-all duration-500'>Explore</button>
         </div>
     )
 }
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -10,16 +10,16 @@ const Welcome = () => {
             <div className="flex flex-col gap-3 items-center">
                 <NavBar />
 
-                {/* <div className="flex flex-col sm:flex-row items-center pt-24 sm:pt-0">
+                <div className="flex flex-col sm:flex-row items-center pt-24 sm:pt-0">
                     <div className="flex-1 flex flex-col items-start gap-5">
                         <h2 className='text-3xl sm:text-5xl lg:text-7xl font-semibold baskervville-regular'>Enhance Your Text Experience</h2>
                         <p className='italic text-base sm:text-lg font-extrabold baskervville-regular-italic text-gray-600'>Try out our language detection, summarization and translation features to make your text processing easier and more efficient.</p>
                         <button className='border border-[#1d4537] bg-[#1d4537] text-white py-3 px-10 font-semibold hover:text-[#1d4537] hover:bg-[#d0d0ce] transition-all duration-500 ease-in-out'>Get Started</button>
                     </div>
                     <img src={bgai} alt="" className='flex-1 w-[400px] sm:w-[300px]' />
-                </div> */}
+                </div>
 
-                <div
+                {/* <div
                     className="flex flex-col sm:flex-row items-center pt-24 sm:pt-0"
                 >
                     <motion.div
@@ -70,7 +70,7 @@ const Welcome = () => {
                         transition={{ duration: 0.8, ease: "easeOut", delay: 0.6 }}
                         viewport={{ once: true }}
                     />
-                </div>
+                </div> */}
 
             </div>
         </div>
