7911224814d55d6bc5c0205363389ad9e888f8f2
jeremiah <bankolejeremiahadeoye@gmail.com>
2025-02-21 23:53:36 +0100

    still working on the translation and summary feature

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
     return (
         <div className='flex justify-between w-full py-4 fixed top-0 left-0 px-3 md:px-6 lg:px-12 bg-[#d0d0ce]'>
             <div onClick={() => navigate("/")} className="road-rage-regular text-4xl flex items-end cursor-pointer">TextPT <span className='relative bottom-2 block font-bold w-2 h-2 bg-[#1d4537] rounded-full'></span></div>
-            <button className='text-[#1d4537] border border-[#1d4537] hover:bg-[#1d4537] hover:text-white px-6 text-lg font-semibold transition-all duration-500'>Explore</button>
+            <button onClick={() => navigate("/workspace")} className='text-[#1d4537] border border-[#1d4537] hover:bg-[#1d4537] hover:text-white px-6 text-lg font-semibold transition-all duration-500'>Explore</button>
         </div>
     )
 }
